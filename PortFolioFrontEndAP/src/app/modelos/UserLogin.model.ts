97c1a3de8768de1f93292ad095007f83d1b73ebd
Roger Roldan <roger349@gmail.com>
2023-05-05 20:54:50 -0300

    commit 1 proyecto final Ap

---
+++ PortFolioFrontEndAP/src/app/modelos/UserLogin.model.ts
@@ -0,0 +1,11 @@
+export class UserLogin {
+    nombre_usuario:string;
+    password_usuario:string;
+
+    constructor(nombre_usuario:string, password_usuario:string) {
+
+        this.nombre_usuario = nombre_usuario;
+        this.password_usuario = password_usuario; 
+          
+    }
+}
